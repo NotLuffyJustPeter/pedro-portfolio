d88703cdc826e67f6973564db9c17588c0383054
NotLuffyJustPeter <pedroromangd@gmail.com>
2026-08-17 14:58:14 -0600

    feat: build portfolio navbar and hero

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+
+  imports: [
+    RouterLink
+  ],
+
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
+  readonly menuOpen = signal(false);
+
+  toggleMenu(): void {
+    this.menuOpen.update(value => !value);
+  }
+
+  closeMenu(): void {
+    this.menuOpen.set(false);
+  }
 }
