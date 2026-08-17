df9f4206da5c4be4f58f1a0a9036f5c638f694b7
NotLuffyJustPeter <pedroromangd@gmail.com>
2026-08-17 14:42:40 -0600

    feat: initialize portfolio structure

---
+++ src/app/sections/what-i-do/what-i-do.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-what-i-do',
+  imports: [],
+  templateUrl: './what-i-do.component.html',
+  styleUrl: './what-i-do.component.scss'
+})
+export class WhatIDoComponent {
+
+}
