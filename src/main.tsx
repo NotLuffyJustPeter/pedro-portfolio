2ed675a04759e5af2e640b490db3feaf452be355
NotLuffyJustPeter <pedroromangd@gmail.com>
2026-08-23 00:53:20 -0600

    style: establish React design system and typography

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
 import { router } from './app/router';
+import '@fontsource-variable/space-grotesk';
+import '@fontsource-variable/inter';
+import '@fontsource/jetbrains-mono/400.css';
+import '@fontsource/jetbrains-mono/500.css';
 
 import './styles/global.scss';
 
